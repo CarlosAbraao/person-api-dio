0c0c4cc1a54e67457d0869ceb683909c90f75f4d
Carlos Abraao <carloschavesnd@hotmail.com>
2022-02-20 15:43:05 -0300

    Atualização de comentário

--- src/main/java/com/carlosdev/personApi/personApi/controller/PersonController.java
+++ src/main/java/com/carlosdev/personApi/personApi/controller/PersonController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//TESTE DE COMMIT
 @RestController
 @RequestMapping ("/api/v1/people")
 public class PersonController {
