e27fb9ff28dcd9eaf08f257188137c35e540dfa7
Carlos Abraao <carloschavesnd@hotmail.com>
2022-02-20 19:17:11 -0300

    configuração do arquivo system.properties-heroku

--- src/main/java/com/carlosdev/personApi/personApi/controller/PersonController.java
+++ src/main/java/com/carlosdev/personApi/personApi/controller/PersonController.java
@@ -4,13 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//TESTE DE COMMIT
+
 @RestController
 @RequestMapping ("/api/v1/people")
 public class PersonController {
 
     @GetMapping
     public String getbook(){
-        return "api Test"; sdsdsdsdssd
+        return "api Test";
     }
 }
